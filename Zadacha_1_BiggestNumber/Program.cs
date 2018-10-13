2ee2d75bbc6a91d346a44dff6ff47dc264122533
Martina <martts00@gmail.com>
2018-10-13 23:12:20 +0300

    initial commit

---
+++ Zadacha_1_BiggestNumber/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha_1_BiggestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+
+            Console.WriteLine("Please, insert first number");
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please, insert second number");
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please, insert third number");
+            c = double.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                if (a > c)
+                {
+                    Console.WriteLine("The biggest number is {0}", a);
+                }
+
+                else
+                {
+                    Console.WriteLine("The biggest number is {0}", c);
+                }
+            }
+            else if (b > c)
+            {
+                Console.WriteLine("The biggest number is {0}", b);
+            }
+            Console.ReadKey(true);
+        }
+    }
+    
+}
